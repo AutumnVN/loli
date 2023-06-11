6b5c0daba3a80a4246162b1f5d79259876548d3b
AutumnVN <htuan03@gmail.com>
2023-06-11 15:54:34 +0700

    Add PlayerHealth

---
+++ src/main/java/autumnvn/ass/mixin/EntityRendererMixin.java
@@ -0,0 +1,53 @@
+package autumnvn.ass.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin<T extends Entity> {
+	@Shadow
+	protected abstract boolean hasLabel(T entity);
+
+	@Shadow
+	protected abstract void renderLabelIfPresent(T entity, Text text, MatrixStack matrices,
+			VertexConsumerProvider vertexConsumers, int light);
+
+	@Inject(method = "render", at = @At("HEAD"))
+	private void onRender(T entity, float yaw, float tickDelta, MatrixStack matrices,
+			VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+		if (entity instanceof PlayerEntity) {
+			String playerName = entity.getName().getString();
+			int health = (int) Math.ceil(getHealth(entity));
+			playerName += "  " + getHealthColor(health) + health + Formatting.RED + " ❤";
+			this.renderLabelIfPresent(entity, Text.of(playerName), matrices, vertexConsumers, light);
+		}
+	}
+
+	private float getHealth(T entity) {
+		return ((LivingEntity) entity).getHealth() + ((LivingEntity) entity).getAbsorptionAmount();
+	}
+
+	private Formatting getHealthColor(int health) {
+		if (health <= 5)
+			return Formatting.RED;
+		if (health <= 10)
+			return Formatting.GOLD;
+		if (health <= 15)
+			return Formatting.YELLOW;
+		if (health <= 20)
+			return Formatting.GREEN;
+		return Formatting.DARK_GREEN;
+	}
+}
