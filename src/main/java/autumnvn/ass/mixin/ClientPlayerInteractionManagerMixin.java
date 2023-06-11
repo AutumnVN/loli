48bd76beef34a993e5616ebd5d3259b63bf4ef4a
AutumnVN <htuan03@gmail.com>
2023-06-11 21:05:21 +0700

    Add NoMineDelay

---
+++ src/main/java/autumnvn/ass/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,15 @@
+package autumnvn.ass.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {
+    @ModifyConstant(method = "updateBlockBreakingProgress", constant = @Constant(intValue = 5))
+    private int miningCooldown(int value) {
+        return 0;
+    }
+}
