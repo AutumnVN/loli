15886fc95d90ac40f13798afb5bdb37ab20ea52e
AutumnVN <htuan03@gmail.com>
2023-06-11 16:51:39 +0700

    Add FullBrightness

---
+++ src/main/java/autumnvn/ass/mixin/LightmapTextureManagerMixin.java
@@ -0,0 +1,16 @@
+package autumnvn.ass.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.render.LightmapTextureManager;
+
+@Mixin(LightmapTextureManager.class)
+public class LightmapTextureManagerMixin {
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(1f);
+    }
+}
