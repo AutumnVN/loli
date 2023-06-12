0250263e2745661a2848ab778a15b8aa2f45b846
AutumnVN <htuan03@gmail.com>
2023-06-12 11:20:59 +0700

    Add TriggerBot

--- src/main/java/autumnvn/ass/ASS.java
+++ src/main/java/autumnvn/ass/ASS.java
@@ -3,9 +3,15 @@ package autumnvn.ass;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult.Type;
 
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -19,18 +25,44 @@ public class ASS implements ModInitializer {
 	public static int deathZ = 0;
 	public static String deathWorld = "";
 	public static boolean mobHealth = false;
-	private static KeyBinding mobHealthKeybind;
+	// private static KeyBinding mobHealthKey;
+	public static boolean triggerBot = false;
+	// private static KeyBinding triggerBotKey;
 
 	@Override
 	public void onInitialize() {
-		mobHealthKeybind = KeyBindingHelper.registerKeyBinding(
+		KeyBinding mobHealthKey = KeyBindingHelper.registerKeyBinding(
 				new KeyBinding("ass.mobHealth", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, "AutumnVN's silly stuffs"));
+		KeyBinding triggerBotKey = KeyBindingHelper.registerKeyBinding(
+				new KeyBinding("ass.triggerBot", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "AutumnVN's silly stuffs"));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (mobHealthKeybind.wasPressed()) {
+			while (mobHealthKey.wasPressed()) {
 				mobHealth = !mobHealth;
 				client.player.sendMessage(
 						Text.literal(mobHealth ? "§aMobHealth is enabled" : "§cMobHealth is disabled"), true);
+			}
+			while (triggerBotKey.wasPressed()) {
+				triggerBot = !triggerBot;
+				client.player.sendMessage(
+						Text.literal(triggerBot ? "§aTriggerBot is enabled" : "§cTriggerBot is disabled"), true);
+			}
+		});
+
+		ClientTickEvents.START_WORLD_TICK.register(clientWorld -> {
+			MinecraftClient client = MinecraftClient.getInstance();
+			if (triggerBot && client.crosshairTarget != null && client.crosshairTarget.getType() == Type.ENTITY
+					&& client.player.getAttackCooldownProgress(0.0f) >= 1.0f) {
+				if (((EntityHitResult) client.crosshairTarget).getEntity() instanceof LivingEntity) {
+					LivingEntity livingEntity = (LivingEntity) ((EntityHitResult) client.crosshairTarget)
+							.getEntity();
+					if (livingEntity.isAttackable()
+							&& (livingEntity.hurtTime == 0 || livingEntity instanceof WitherEntity)
+							&& livingEntity.isAlive()) {
+						client.interactionManager.attackEntity(client.player, livingEntity);
+						client.player.swingHand(Hand.MAIN_HAND);
+					}
+				}
 			}
 		});
 	}
